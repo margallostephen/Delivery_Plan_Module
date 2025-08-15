8c2fd8929662346c44257c4588fa392c85874493
margallostephen <stephenmargallo04@gmail.com>
2025-08-15 11:40:07 +0800

    Remove comment

--- ajax/exportExcel.js
+++ ajax/exportExcel.js
@@ -23,7 +23,7 @@ function exportExcel(tableData, importDatetime) {
         })
     }).then(response => {
         if (!response.ok) throw new Error("Network response was not ok.");
-        return response.blob(); // get the binary blob
+        return response.blob();
     }).then(blob => {
         const url = window.URL.createObjectURL(blob);
         const a = document.createElement('a');
