c2713bbec2e3f436ce9cc928129d4312818da55b
margallostephen <stephenmargallo04@gmail.com>
2025-08-20 10:47:23 +0800

    refactor: add footer partial for improved UI structure

---
+++ partials/footer.php
@@ -0,0 +1,12 @@
+<footer class="footer">
+    <div class="footer-inner">
+        <div class="footer-content">
+            <span class="bigger-120 blue bolder">
+                <?php echo "FG MANAGEMENT SYSTEM "; ?>
+            </span>
+            <span class="bigger-120">
+                © March 2023
+            </span>
+        </div>
+    </div>
+</footer>
